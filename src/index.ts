ca14f66908d6abb339b5c95c32883924c935c702
Syed Abdullah <Syed.Abdullah@agoda.com>
2018-10-08 17:29:10 +0700

    updated 19

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-// require.config({
-//     baseUrl: "./",
-//     paths: {
-//       "org/cometd": 'node_modules/cometd-jquery/org/cometd',
-//       "jquery.cometd": 'node_modules/cometd-jquery/jquery/jquery.cometd',
-//     }
-// });
-require('jquery');
-require("cometd")
+
+import * as  jquery from "jquery";
+
+import  "cometd";
+import  "cometd-jquery";
+import "./widgets.min";
 import { Genesys, MyWindow, Main,Widgets} from './ss';
 let myWindow = window as MyWindow;
 declare var require: any;
 if(!myWindow._genesys)myWindow._genesys = { text: "hello" } as Genesys;
 if(!myWindow._gt)myWindow._gt = [];
-require("./widgets.min");
+
 const _Genesys: Genesys =  myWindow._genesys;
 const mywidget = {
     _Genesys: _Genesys,
